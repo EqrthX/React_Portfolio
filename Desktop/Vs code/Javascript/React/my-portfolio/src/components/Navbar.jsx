bde6d0e4c7638b8b4461f243214bb33147b0468d
EqrthX <nontprawitchkung@gmail.com>
2025-02-13 18:58:33 +0700

    fix responsive

--- Desktop/Vs code/Javascript/React/my-portfolio/src/components/Navbar.jsx
+++ Desktop/Vs code/Javascript/React/my-portfolio/src/components/Navbar.jsx
@@ -14,14 +14,14 @@ const Navbar = () => {
                 <span className='text-3xl font-bold'>Earth</span>
             </div>
             
-            <div className='md:hidden'>
+            <div className='sm:hidden -scale-z-90'>
                <button onClick={() => setIsOpen(!isOpen)} className='transition-transform duration-200 ease-in-out'>
                     {isOpen ? <X size={32} className='rotate-180'/> : <AlignJustify size={32} className='rotate-0'/>}
                </button>
             </div>
             
             {/* แสดงสำหรับจอใหญ่ */}
-            <ul className='font-medium  gap-5 text-lg uppercase tracking-wider hidden md:flex'>
+            <ul className='font-medium  gap-5 text-lg uppercase tracking-wider hidden sm:flex'>
                 {["home", "skills", "project", "contact"].map((item, index) => (
                     <li key={index} 
                     className='transition-color duration-200 ease-out hover:text-amber-700 border-b-4 border-transparent hover:border-amber-700 cursor-pointer'>
@@ -35,7 +35,9 @@ const Navbar = () => {
 
             {/* แสดงสำหรับมือถือ */}
 
-            <div className={`absolute top-15 right-0 bg-[#BDD2E9] rounded-lg p-1 transition-transform duration-300 ease-in-out ${isOpen ? "translate-x-0" : "translate-x-full"} md:hidden`}>
+            <div 
+            className={`absolute top-15 right-0 bg-[#BDD2E9] rounded-lg p-1 transition-transform duration-300 
+            ease-in-out ${isOpen ? "translate-x-0" : "translate-x-full"} sm:hidden block`}>
                 <ul className='font-medium text-lg uppercase tracking-wider flex flex-col items-center py-4'>
                     {["home", "skills", "project", "contact"].map((item, index) => (
                         <li
