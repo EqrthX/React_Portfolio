2e9a7487589d5574b78cea0cd7862a633767d781
EqrthX <nontprawitchkung@gmail.com>
2025-02-13 18:23:09 +0700

    Update Responvie

--- Desktop/Vs code/Javascript/React/my-portfolio/src/components/Hero.jsx
+++ Desktop/Vs code/Javascript/React/my-portfolio/src/components/Hero.jsx
@@ -2,17 +2,20 @@ import earth from '../assets/earth.jpg'
 
 const Hero = () => {
   return (
-    <section id="hero" className='container min-h-screen flex justify-center items-center top-0'>
+    <section id="home" className='container min-h-screen flex flex-col md:flex-row justify-center items-center gap-8 p-4'>
 
-      <div className='w-1/2 flex items-center justify-center'>
+      {/* จอใหญ่ */}
+      <div className='w-full md:w-1/2 flex flex-col items-center md:items-center text-center md:text-left'>
         <div className=''>
             <h1 className='text-[40px] font-black mb-2'>Hello, I&apos;m Nontprawitch Saetang</h1>
             <p className='text-lg font-semibold'>Welcome to my portfolio</p>
         </div>
       </div>
 
-      <div className='w-1/2 flex justify-center'>
-            <img src={earth} className='w-full h-auto max-w-[500px] mx-auto rounded-full border-7 border-b-indigo-800 border-l-cyan-800 border-t-emerald-800 border-r-fuchsia-800 shadow-2xl'/>
+      <div className='w-full md:w-1/2 flex justify-center '>
+            <img 
+            src={earth} 
+            className='w-full max-w-[300px] md:max-w-[500px] mx-auto rounded-full border-7 border-b-indigo-800 border-l-cyan-800 border-t-emerald-800 border-r-fuchsia-800 shadow-2xl'/>
       </div>
 
     </section>
--- Desktop/Vs code/Javascript/React/my-portfolio/src/components/Navbar.jsx
+++ Desktop/Vs code/Javascript/React/my-portfolio/src/components/Navbar.jsx
@@ -1,7 +1,11 @@
-import { Earth } from 'lucide-react'
+import { AlignJustify, Earth, X } from 'lucide-react'
+import { useState } from 'react'
 import { Link } from 'react-scroll'
 
 const Navbar = () => {
+
+    const [isOpen, setIsOpen] = useState(false)
+
   return (
     <nav className='bg-[#BDD2E9] flex items-center w-full text-black sticky top-0 z-50'>
         <div className='container max-w-screen-xl mx-auto flex items-center justify-between px-4 py-3'>
@@ -9,54 +13,50 @@ const Navbar = () => {
                 <Earth size={52} />
                 <span className='text-3xl font-bold'>Earth</span>
             </div>
-
+            
+            <div className='md:hidden'>
+               <button onClick={() => setIsOpen(!isOpen)} className='transition-transform duration-200 ease-in-out'>
+                    {isOpen ? <X size={32} className='rotate-180'/> : <AlignJustify size={32} className='rotate-0'/>}
+               </button>
+            </div>
+            
+            {/* แสดงสำหรับจอใหญ่ */}
             <ul className='font-medium  gap-5 text-lg uppercase tracking-wider hidden md:flex'>
-                
-                <li className='transition-color duration-200 ease-out hover:text-amber-700 border-b-4 border-transparent hover:border-amber-700 cursor-pointer fo'>
-                    <Link to='hero' 
-                    smooth={true} 
-                    duration={500} 
-                    spy={true} 
-                    offset={-80}
-                    isDynamic={true}>
-                    Home
+                {["home", "skills", "project", "contact"].map((item, index) => (
+                    <li key={index} 
+                    className='transition-color duration-200 ease-out hover:text-amber-700 border-b-4 border-transparent hover:border-amber-700 cursor-pointer'>
+                    <Link to={item} smooth={true} duration={500} spy={true} offset={-80} isDynamic={true}>
+                      {item.charAt(0).toUpperCase() + item.slice(1)}
                     </Link>
-                </li>
-                
-                <li className='transition-color duration-200 ease-out hover:text-amber-700 border-b-4 border-transparent hover:border-amber-700 cursor-pointer'>
-                    <Link to='skills' 
-                    smooth={true} 
-                    duration={500} 
-                    offset={-80}
-                    spy={true}
-                    isDynamic={true}>
-                    Skills
-                    </Link>
-                </li>
-
-                <li className='transition-color duration-200 ease-out hover:text-amber-700 border-b-4 border-transparent hover:border-amber-700 cursor-pointer'>
-                    <Link to='project'
-                    smooth={true} 
-                    duration={500} 
-                    spy={true}
-                    offset={-80}
-                    isDynamic={true}>
-                    Project
-                    </Link>
-                </li>
-
-                <li className='transition-color duration-200 ease-out hover:text-amber-700 border-b-4 border-transparent hover:border-amber-700 cursor-pointer'>
-                    <Link to='contact'
-                    smooth={true}
-                    duration={500}
-                    spy={true}
-                    offset={-80}
-                    isDynamic={true}>
-                    Contact Us
-                    </Link>
-                </li>
+                  </li>
+                ))}
                 
             </ul>
+
+            {/* แสดงสำหรับมือถือ */}
+
+            <div className={`absolute top-15 right-0 bg-[#BDD2E9] rounded-lg p-1 transition-transform duration-300 ease-in-out ${isOpen ? "translate-x-0" : "translate-x-full"} md:hidden`}>
+                <ul className='font-medium text-lg uppercase tracking-wider flex flex-col items-center py-4'>
+                    {["home", "skills", "project", "contact"].map((item, index) => (
+                        <li
+                            key={index}
+                            className='py-3 w-full text-center transition-colors hover:bg-amber-500 rounded-lg '
+                        >
+                            <Link
+                                to={item}
+                                smooth={true}
+                                duration={500}
+                                spy={true}
+                                offset={-80}
+                                isDynamic={true}
+                                onClick={() => setIsOpen(false)} //กดแล้วปิดเมนู
+                            >
+                            {item.charAt(0).toUpperCase() + item.slice(1)}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
     </nav>
   )
